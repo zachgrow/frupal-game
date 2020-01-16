0056767821f0ee44350d9c10f551fc2d1e8b8741
MisterEggnog <MisterEggnog@outlook.com>
2020-01-16 13:32:52 -0800

    added stuff

--- engine.cpp
+++ engine.cpp
@@ -7,8 +7,26 @@
 
 struct Game {
 
+	Game() noexcept;
+
+	void loop();
+
 };
 
 int main(int argc, char** argv)
 {
+	// Read argv for json config file
+	// Flags for map size?
 }
+
+Game::Game() noexcept
+{
+}
+
+void Game::loop()
+{
+	// Fetch player action
+	// Perform action
+	// Write result
+}
+

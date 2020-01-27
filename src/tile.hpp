aa36e36023441e5e029f80b3bbaf64bedafc8cff
dleep17 <dleep@pdx.edu>
2020-01-27 12:12:05 -0800

    "Testing committing. Slight change to tile.hpp"

--- src/tile.hpp
+++ src/tile.hpp
@@ -1,7 +1,12 @@
 #ifndef FRUPALGAME_SRC_TILE_HPP_INCLUDED
 #define FRUPALGAME_SRC_TILE_HPP_INCLUDED
 
+#include <string>
+#include <vector>
+
 class Tile {
+    public:
+    std::string obstacle;
 };
 
 #endif // FRUPALGAME_SRC_TILE_HPP_INCLUDED

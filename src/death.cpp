83ddfa959a1dfd25227393542e0f0a8b50cd4598
Zach Grow <zgrow@pdx.edu>
2020-03-09 14:30:38 -0700

    Corrected formatting of victory and death messages

--- src/death.cpp
+++ src/death.cpp
@@ -8,15 +8,16 @@ Death_event::Death_event(bool is_mud)
 
 void Death_event::greeting()
 {
-	if (is_mud_)
-		writeMsg("As you walk through the mud, your boot get's stuck.\n"
-				 "As you try to pull your boot out, you realize you have began to sink!\n"
-				 "You futilly scream for help as you slow sink below the surface.\n"
-				 "Later, another adventure finds a bag of supplies in this swamp...\n");
-	else
-		writeMsg("As you walk along, you begun to shake.\n"
-				 "You realize your blood suger is low!\n"
-				 "Guess its time to go home, we can search more tomorrow.\n");
+	if (is_mud_) {
+		writeMsg("As you walk through the mud, your boot gets stuck.");
+		writeMsg("As you try to pull your boot out, you realize you have began to sink!");
+		writeMsg("You scream for help to no avail as you slowly sink below the surface.");
+		writeMsg("Later, another adventurer finds a bag of supplies in this swamp...");
+	} else {
+		writeMsg("As you walk along, you begin to shake.");
+		writeMsg("You realize your blood sugar is low!");
+		writeMsg("Guess it's time to go home, we can search more tomorrow.");
+	}
 }
 
 void Death_event::react_to_player()
--- src/victory.cpp
+++ src/victory.cpp
@@ -3,10 +3,10 @@
 
 void Victory::greeting()
 {
-	writeMsg("OWW! You've stubbed your toe on a rock!\n"
-		"Hey, that rock is kinda shiny?\n"
-		"Wait a minute...\n"
-		"You did it! You found the jewel of Frupal!\n");
+	writeMsg("OWW! You've stubbed your toe on a rock!");
+	writeMsg("Hey, that rock is kinda shiny?");
+	writeMsg("Wait a minute...");
+	writeMsg("You did it! You found the jewel of Frupal!");
 }
 
 void Victory::react_to_player()
